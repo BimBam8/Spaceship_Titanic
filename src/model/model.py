9a4b8a08d9e7fc60ea0fa89958f704808499b255
Sem <semaenenter@gmail.com>
2026-05-31 15:31:11 +0000

    added load and save

--- src/model/model.py
+++ src/model/model.py
@@ -1,6 +1,8 @@
 from src.features.features import build_feature_engineer, build_column_transformer
 from src.features.pipeline import build_pipeline
 from src.data_import.data import get_train_val_test_clean
+import pickle
+
 
 def train(X, y):
     pipeline = build_pipeline(build_feature_engineer(), build_column_transformer())
@@ -8,7 +10,19 @@ def train(X, y):
     return pipeline
 
 
-if __name__=="__main__":
+def save(model, path: str):
+    with open(path, "wb") as f:
+        pickle.dump(model, f)
+
+
+def load(path: str):
+    with open(path, "rb") as f:
+        return pickle.load(f)
+
+
+if __name__ == "__main__":
     X_train, X_val, X_test, y_train, y_val, y_test = get_train_val_test_clean()
-    model = train(X_train, y_train)
+    # model = train(X_train, y_train)
+    model = load("mlruns/1.pkl")
     print(model.predict(X_train))
+    # save(model, "mlruns/1.pkl")
